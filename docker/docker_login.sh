0756fbdcc2964b3aa261fe187737a3085c1434a6
A$c3t1c <bugb0untypropentest+esdeg@gmail.com>
2025-02-07 11:34:09 +0200

    docker-login-ghcr
    docker-login-ghcr

---
+++ docker/docker_login.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REGISTRY_URL="https://index.docker.io/v1/"
+#REGISTRY_URL="https://registry-1.docker.io/v2/"
+
+# Docker credentials
+USERNAME="your_docker_user"
+PASSWORD="your_docker_password"
+
+# Part 0.1
+GITOKEN="your_token_here"
+docker login $REGISTRY_URL -u $USERNAME -p $PASSWORD
+docker login ghcr.io -u $USERNAME --password $GITOKEN
+
+# Infocheck
+docker info | grep Username
+exit 0;
