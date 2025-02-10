435089f9ca41e4ca2803642562026521ac4e3d6e
asc3t1c <bugb0untypropentest+esdeg@gmail.com>
2025-02-10 10:49:41 +0200

    Add files via upload

--- tools/single-builder.sh
+++ tools/single-builder.sh
@@ -1,1 +1,9 @@
-soon
+#!/usr/bin/bash
+# ENV
+env = "24.04.1"
+docker = "f0rc3ps"
+
+echo "Give the image name"
+read img 
+docker buildx build --platform linux/amd64,linux/arm64 -t $docker/$img:$env --push .
+exit 0;
