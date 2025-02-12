f56181305e1475992078c71e5003011fa5ee9b1d
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-12 13:54:31 +0200

    Add files via upload

--- tools/single-builder.sh
+++ tools/single-builder.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 # ENV
 env = "24.04.1"
-docker = "f0rc3ps"
+docker_repo = "f0rc3ps"
 
 echo "Give the image name"
 read img 
-docker buildx build --platform linux/amd64,linux/arm64 -t $docker/$img:$env --push .
+# docker buildx build --builder=container --platform=linux/amd64,linux/arm64 -t f0rc3ps/nginx:24.04.1 --push .
+docker buildx build --builder=container --platform=linux/amd64,linux/arm64 -t $docker_repo/$img:$env --push .
 exit 0;
