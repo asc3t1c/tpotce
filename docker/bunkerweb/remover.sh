52b015ec4e3bdbf3bb7ee4c4a3979af9fb1ef49d
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-16 01:43:38 +0200

    Update remover.sh

--- docker/bunkerweb/remover.sh
+++ docker/bunkerweb/remover.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 docker rmi -f mariadb:10.10 bunkerity/bunkerweb:1.5.10 bunkerity/bunkerweb-scheduler:1.5.10 bunkerity/bunkerweb-ui:1.5.10 tecnativa/docker-socket-proxy:nightly
+docker rmi -f mariadb:11.4 bunkerity/bunkerweb:1.6.0 bunkerity/bunkerweb-scheduler:1.6.0 bunkerity/bunkerweb-ui:1.6.0 tecnativa/docker-socket-proxy:nightly
 # docker system prune -a 
 docker images -a
+
