fafe31233cc7b2134a0f59287206de92f0d35458
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 16:32:42 +0200

    Update update.sh

--- docker/bunkerweb/update.sh
+++ docker/bunkerweb/update.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 docker stop bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1 bunkerweb-bw-docker-1 bunkerweb-bw-db-1
-docker rm bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
-docker rmi mariadb:10.10
+docker update bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 rm -rf docker-compose.yml.*
 rm -rf docker-compose.yml*
 wget https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/docker-compose.yml
