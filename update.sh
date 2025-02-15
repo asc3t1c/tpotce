bfe4c242d3b86f5999d77b47ed34f0d7672551a2
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 10:46:29 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -169,14 +169,16 @@ function fuUPDATER () {
 	fuREMOVEOLDIMAGES "ghcr.io/asc3t1c/*:dev"
 	fuREMOVEOLDIMAGES "f0rc3ps/*:24.04"
 	fuREMOVEOLDIMAGES "ghcr.io/asc3t1c/*:24.04"
-        ############################
+        
+	############################
 	# Update outside resources # 
 	############################
-	echo "Updating of outside SRC..."
-        sleep 3;
-	cd docker/bunkerweb/
-	docker compose up -d
-	echo
+	#echo "Updating of outside SRC..."
+        #sleep 3;
+	#cd docker/bunkerweb/
+	#docker compose up -d
+	
+ 	echo
 	echo "### If you made changes to docker-compose.yml please ensure to add them again."
 	echo "### We stored the previous version as backup in $myARCHIVE."
 	echo "### Some updates may need an import of the latest Kibana objects as well."
