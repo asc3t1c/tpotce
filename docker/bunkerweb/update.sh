69c824f44ce6d2eb2c1456c43599f54f6d5e047a
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 13:26:32 +0200

    Update update.sh

--- docker/bunkerweb/update.sh
+++ docker/bunkerweb/update.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-docker kill $(docker ps -q)
+docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
+docker rmi bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 docker compose up -d
