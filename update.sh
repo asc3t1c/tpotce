db6b401060e98e81f3b4bc4801d8f2ee6bbc0e99
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 15:55:02 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -208,6 +208,16 @@ if [ "$1" != "-y" ]; then
   exit
 fi
 
+echo "Updating a BunkerWeb please wait..." 
+sleep 5;
+rm -rf update.sh.*
+cd ../bunkerweb/
+rm -rf update.sh*
+wget https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/update.sh
+bash update.sh
+cd 
+sleep 3;
+
 fuCHECK_VERSION
 fuCHECKINET "https://index.docker.io https://github.com"
 fuSTOP_TPOT
