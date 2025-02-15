6dafb1d0fe1f9885bd1da76b8f77401787a9c4cf
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-16 00:01:45 +0200

    Update remover.sh

--- docker/bunkerweb/remover.sh
+++ docker/bunkerweb/remover.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
-docker system prune -a 
+docker rmi -f bunkerity/bunkerweb:1.5.10 bunkerity/bunkerweb-scheduler:1.5.10 bunkerity/bunkerweb-ui:1.5.10
+# docker system prune -a 
 docker images -a
