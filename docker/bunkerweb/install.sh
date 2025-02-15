b70ee261915e77a932483ed70932d923210181f0
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 16:03:11 +0200

    Update install.sh

--- docker/bunkerweb/install.sh
+++ docker/bunkerweb/install.sh
@@ -3,6 +3,3 @@ cd bunkerweb
 wget https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/docker-compose.yml
 docker compose up -d
 sleep 3;
-
-# docker stop $(docker ps -a -q)
-# docker start $(docker ps -a -q)
