1fa2b4f74cd222e378a44ef16c959afdb76befd4
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 14:16:28 +0200

    Update update.sh

--- docker/bunkerweb/update.sh
+++ docker/bunkerweb/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
-docker stop $(docker ps -a -q)
+# docker stop $(docker ps -a -q)
 docker compose up -d
 sleep 3;
-docker start $(docker ps -a -q)
-
+# docker start $(docker ps -a -q)
