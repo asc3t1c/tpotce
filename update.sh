2efa50c7c06ada261acbad453f616e4ddd089f52
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-15 15:30:46 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 echo "Updating a BunkerWeb please wait..." 
 sleep 5;
+rm -rf update.sh.*
 cd ../bunkerweb/
 rm -rf updatate.sh*
 wget https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/update.sh
