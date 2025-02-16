8302fbf7158466242e70b8a521411ad189018391
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-16 17:44:42 +0200

    Delete docker/bunkerweb directory

--- docker/bunkerweb/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-docker stop bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1 bunkerweb-bw-docker-1 bunkerweb-bw-db-1
-docker update bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-docker-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
-rm -rf docker-compose.yml.*
-rm -rf docker-compose.yml*
-wget https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/docker-compose.yml
-docker compose up -d
-sleep 3;
