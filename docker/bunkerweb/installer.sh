b48088e2272424beb2a62ce1139a409baa4fafd7
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-17 13:33:03 +0200

    Update installer.sh

--- docker/bunkerweb/installer.sh
+++ docker/bunkerweb/installer.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+# cd bunkerweb
+rm -rf docker-compose.yml*
+rm -rf docker-compose.yml.*
+wget -qO docker-compose.yml https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/docker-compose.yml
 docker compose up -d
-docker compose down -v
-docker compose up -d
+#docker stop bunkerweb-bw-docker-1 bunkerweb-bw-db-1 bunkerweb-bunkerweb-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
+#docker compose up -d
+sleep 3;
