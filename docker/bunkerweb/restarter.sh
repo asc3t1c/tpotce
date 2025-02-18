4503cc43f7ecd076870a3b0d6f1342c52a73e14f
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-18 10:27:46 +0200

    Update restarter.sh

--- docker/bunkerweb/restarter.sh
+++ docker/bunkerweb/restarter.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
+docker kill bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 docker restart bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
+
