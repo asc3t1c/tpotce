a27ec03dfea99c40b42f5f76807004ad906080ef
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-19 13:27:31 +0200

    Update installer.sh

--- docker/bunkerweb/installer.sh
+++ docker/bunkerweb/installer.sh
@@ -4,9 +4,8 @@ rm -rf docker-compose.yml*
 rm -rf docker-compose.yml.*
 ###docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 wget -qO docker-compose.yml https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/docker-compose.yml
-sudo docker compose up -d
-sudo docker compose down -v
-sudo docker compose up -d
+docker compose up -d
+
 ###docker stop bunkerweb-bw-docker-1 bunkerweb-bw-db-1 bunkerweb-bunkerweb-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 ###docker compose up -d
 ###systemctl start tpot.service
