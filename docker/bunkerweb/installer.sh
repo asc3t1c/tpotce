a2fe1c3f6bfd5df3f905f3031eae63ba31e4463f
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-19 11:04:46 +0200

    Update installer.sh

--- docker/bunkerweb/installer.sh
+++ docker/bunkerweb/installer.sh
@@ -2,11 +2,13 @@
 # cd bunkerweb
 rm -rf docker-compose.yml*
 rm -rf docker-compose.yml.*
-# docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
+###docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
 wget -qO docker-compose.yml https://raw.githubusercontent.com/asc3t1c/tpotce/refs/heads/master/docker/bunkerweb/docker-compose.yml
 docker compose up -d
-#docker stop bunkerweb-bw-docker-1 bunkerweb-bw-db-1 bunkerweb-bunkerweb-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
-#docker compose up -d
-systemctl start tpot.service
+docker compose down -v
+docker compose up -d
+###docker stop bunkerweb-bw-docker-1 bunkerweb-bw-db-1 bunkerweb-bunkerweb-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
+###docker compose up -d
+###systemctl start tpot.service
 echo "WARNING: DON'T RUN INSTALLER AGAIN OVER THE CURRENT INSTALLATION!"
 sleep 3;
