be39d7c14fd790d998b44a644d912c698b025973
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-20 07:55:43 +0200

    Delete docker/bunkerweb directory

--- docker/bunkerweb/ONLY-first-time-starter.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-docker compose up -d
-docker compose down -v
-docker compose up -d
--- docker/bunkerweb/stopper.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-docker stop bunkerweb-bunkerweb-1 bunkerweb-bw-db-1 bunkerweb-bw-scheduler-1 bunkerweb-bw-ui-1
