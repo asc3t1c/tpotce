571eb78936302e43482b38b0d189dde3f7e0a8e8
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-21 20:58:37 +0200

    Update peppermint.sh

--- docker/peppermint/peppermint.sh
+++ docker/peppermint/peppermint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 # choice userbane
 echo "What username you want to use for peppermint ..."
-read user
-sed -i 's/user/$user/g' docker-compose.yml
+read your_user
+sed -i 's/user/$your_user/g' docker-compose.yml
 # Choice password
 echo "What password you want to use for peppermint ..."
-read password
-sed -i 's/password/$password/g' docker-compose.yml
+read your_password
+sed -i 's/password/$your_password/g' docker-compose.yml
 
 # Install
 echo "### Installing peppermint ..."
