6b1f14a635402ec80ad54b2a610e11ff918f1932
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-22 10:02:35 +0200

    Update peppermint_update.sh

--- docker/peppermint/peppermint_update.sh
+++ docker/peppermint/peppermint_update.sh
@@ -3,5 +3,6 @@
 echo "### Updating peppermint ..."
 sleep 5;
 docker stop peppermint_postgres peppermint
-sudo docker compose -f docker-compose.yml down -v
+# Do not edit here, if you don't know what it is!
+# sudo docker compose -f docker-compose.yml down -v
 sudo docker compose -f docker-compose.yml up -d
