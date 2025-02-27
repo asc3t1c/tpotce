9a185b244a175dc65f56e9a34fca8de22f6c6f6d
nu11secur1ty <venvaropt@gmail.com>
2025-02-27 19:48:57 +0200

    Up-migration

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -1,1 +1,5 @@
-dafdsfsdfdsfs
+#!/usr/bin/bash
+echo "Give a path to migrate..."
+read path
+cp -avr CHANGELOG.md compose/  docker-compose.yml .env env.example genuserwin.ps1 install.sh  uninstall.sh  version \
+	CITATION.cff deploy.sh docker/ dps.ps1 genuser.sh installer/ LICENSE SECURITY.md update.sh $path
