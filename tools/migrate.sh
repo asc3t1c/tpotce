055c1524065c1d45998043366b7480c6f4778638
asc3t1c <bugb0untypropentest@gmail.com>
2025-02-27 20:00:41 +0200

    Update migrate.sh

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 echo "Give a path to migrate..."
 read path
-cp -avr CHANGELOG.md compose/ docker-compose.yml .env env.example genuserwin.ps1 install.sh uninstall.sh version \
+cp -avr CHANGELOG.md compose/ docker-compose.yml genuserwin.ps1 install.sh uninstall.sh version \
 	CITATION.cff deploy.sh docker/ dps.ps1 genuser.sh installer/ LICENSE SECURITY.md update.sh $path
